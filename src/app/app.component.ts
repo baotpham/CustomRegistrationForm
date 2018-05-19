3d4f222479a0573be37da8f4be2f297540e657a0
Bao Thien Pham <baotpham7@gmail.com>
2018-05-19 18:16:38 -0400

    changed title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  title = 'CVCF Youth Retreat 2018 Registration Form';
+
+  constructor(){
+
+  }
 }
