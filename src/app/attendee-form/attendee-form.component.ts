25da412406c7f65cf2d6be001e524098d7152e12
John Ha <jha@ceiamerica.com>
2018-05-23 22:05:46 -0400

    Fixed pagination button styles

--- src/app/attendee-form/attendee-form.component.ts
+++ src/app/attendee-form/attendee-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Attendee } from '../Models/Attendee';
+import { Attendee } from '../models/Attendee';
 
 @Component({
   selector: 'app-attendee-form',
