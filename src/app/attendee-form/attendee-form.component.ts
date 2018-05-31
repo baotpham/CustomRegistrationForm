df2bca7233401c00ea580276ee73467f274a8102
John Ha <jha@ceiamerica.com>
2018-05-31 09:58:12 -0400

    Convert from Template forms to React forms

--- src/app/attendee-form/attendee-form.component.ts
+++ src/app/attendee-form/attendee-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Attendee } from '../Models/Attendee';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators, FormBuilder, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-attendee-form',
@@ -7,26 +8,64 @@ import { Attendee } from '../Models/Attendee';
   styleUrls: ['./attendee-form.component.scss']
 })
 export class AttendeeFormComponent implements OnInit {
+  attendeeForm: FormGroup;
 
-  models = [];
+  first_name = new FormControl("", Validators.required);
+  last_name = new FormControl("", Validators.required);
+  t_shirt = new FormControl("", Validators.required);
+  gender = new FormControl("", Validators.required);
+  age = new FormControl("", Validators.required);
+  medical = new FormControl("", Validators.required);
+
+  forms = [];
   sizes = ['S', 'M', 'L', 'XL', 'XXL'];
   genders = ['Male', 'Female']
   //model = new Attendee('John','Ha', this.sizes[1], 'Male', 23, 'N/A');
-  model = new Attendee("","","","",0,"");
-  
+  model = new Attendee("", "", "", "", 0, "");
+
   submitted = false;
-  onSubmit() { this.submitted = true; }
 
   // Makes sure form is binding with model correctly when "{{diagnostic}}" is placed in form.
   // get diagnostic() { return JSON.stringify(this.model); }
 
-  addAttendee(){
-    this.models.push(this.model);
+  addAttendee() {
+    if (this.attendeeForm.valid) {
+      this.forms.push(this.model);
+      console.log("Adding new attendee.");
+      this.attendeeForm.reset();
+      this.model = new Attendee("", "", "", "", 0, "");
+    }
   }
 
-  constructor() { }
+  constructor(fb: FormBuilder) {
+    this.attendeeForm = fb.group({
+      "first_name": this.first_name,
+      "last_name": this.last_name,
+      "t_shirt": this.t_shirt,
+      "gender": this.gender,
+      "age": this.age,
+      "medical": this.medical
+    });
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log("model-based form submitted");
+    console.log(this.attendeeForm);
+  }
+
+  fullUpdate() {
+    this.attendeeForm.setValue({ first_name: 'Partial', last_name: 'monkey', t_shirt: 'M', gender: 'Male', 
+      age: 12, medical: 'yes' });
+  }
+
+  partialUpdate() {
+    this.attendeeForm.patchValue({ age: this.forms.length });
+  }
+  reset() {
+    this.attendeeForm.reset();
   }
 
 }
